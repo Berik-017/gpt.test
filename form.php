117a09f32425e9f02d733a02b03b26a3396a60d1
Berik-017 <beka11114444@gmail.com>
2026-04-09 16:43:45 +0500

    Delete form.php

--- form.php
+++
@@ -1,5 +0,0 @@
-<form method="POST">
-    <input type="text" name="name" placeholder="Имя">
-    <input type="number" name="age" placeholder="Возраст">
-    <button type="submit">Отправить</button>
-</form>
